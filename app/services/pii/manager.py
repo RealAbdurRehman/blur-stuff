f87e2964451afcf0fb55599ef6c5183f5ba57fd5
RealAbdurRehman <claudethebitplayer@gmail.com>
2026-07-30 14:27:19 +0500

    Add URL support

--- app/services/pii/manager.py
+++ app/services/pii/manager.py
@@ -1,4 +1,5 @@
 from .detectors.email import EmailDetector
+from .detectors.url import UrlDetector
 from .detectors.phone import PhoneDetector
 from .detectors.card import CardDetector
 from .detectors.regex import RegexDetector
@@ -11,6 +12,7 @@ class DetectorManager:
     def __init__(self):
         self.detectors = [
             EmailDetector(),
+            UrlDetector(),
             PhoneDetector(),
             CardDetector(),
             RegexDetector(),
