285d91407a536bf4848fe7d6c7adfcbf913af2ef
RealAbdurRehman <claudethebitplayer@gmail.com>
2026-08-01 13:54:27 +0500

    Clean up pixelate.py

--- app/services/effects/pixelate.py
+++ app/services/effects/pixelate.py
@@ -3,11 +3,11 @@ import cv2
 from .apply import apply_effect
 
 
-def pixelate_regions(image, boxes, padding, fade_ratio, blocks=8):
+def pixelate_regions(image, boxes, padding=0, fade_ratio=0.15, blocks=8):
     def pixelate(roi):
         h, w = roi.shape[:2]
         small = cv2.resize(roi, (blocks, blocks), interpolation=cv2.INTER_LINEAR)
 
         return cv2.resize(small, (w, h), interpolation=cv2.INTER_NEAREST)
 
-    return apply_effect(image, boxes, pixelate, padding=padding, fade_ratio=fade_ratio)
+    return apply_effect(image, boxes, pixelate, padding, fade_ratio)
