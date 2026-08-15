bd7e1d48b7e5d1a30a15cd544b4d1ed2bbc82d96
RealAbdurRehman <claudethebitplayer@gmail.com>
2026-08-15 14:12:33 +0500

    Update run.py

--- run.py
+++ run.py
@@ -1,11 +1,9 @@
 import os
 
-os.environ["FLAGS_use_mkldnn"] = "0"
-os.environ["FLAGS_use_onednn"] = "0"
-
 from app import create_app
 
 app = create_app()
 
 if __name__ == "__main__":
     app.run(debug=True)
+a
