01ffce38ac9e003c0466d12c0114009326fe636b
RealAbdurRehman <claudethebitplayer@gmail.com>
2026-08-15 13:08:51 +0500

    Update run.py

--- run.py
+++ run.py
@@ -1,17 +1,9 @@
 import os
 
 os.environ["FLAGS_use_mkldnn"] = "0"
+os.environ["FLAGS_use_onednn"] = "0"
 
 import paddle
-
-print("FLAGS_use_mkldnn:", paddle.get_flags(["FLAGS_use_mkldnn"]))
-
-print("=== PADDLE DEBUG ===")
-print("Version:", paddle.__version__)
-print("Device:", paddle.device.get_device())
-print("Paddle:", paddle.__file__)
-
-from app import create_app
 
 app = create_app()
 
